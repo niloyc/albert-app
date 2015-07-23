17d70f71683b2fc9135d3426b9cf965be5a41b8b
BradNicolle <bradnicolle@gmail.com>
2015-07-23 13:53:04 +0930

    Added update button, tested on Albert

--- examples/AlbertNetwork/src/com/bradnicolle/albertnetwork/MainActivity.java
+++ examples/AlbertNetwork/src/com/bradnicolle/albertnetwork/MainActivity.java
@@ -8,32 +8,52 @@ import retrofit.client.Response;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private TextView textView;
+	private Button updateButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		RestClient.get().getCustomers(new Callback<List<Customer>>() {
+		textView = (TextView) findViewById(R.id.textview);
+		updateButton = (Button) findViewById(R.id.updateButton);
+
+		updateButton.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void failure(RetrofitError arg0) {
-				Log.i("App", "FAILED!!!!!" + arg0);
-			}
+			public void onClick(View v) {
+				RestClient.get().getCustomers(new Callback<List<Customer>>() {
 
-			@Override
-			public void success(List<Customer> customerResponse,
-					Response response) {
-				for (Customer c : customerResponse) {
-					Log.i("App", "ID: " + c.getId());
-					Log.i("App", "Name: " + c.getName());
-					Log.i("App", "Points: " + c.getPoints());
-				}
-			}
+					@Override
+					public void failure(RetrofitError arg0) {
+						Log.i("App", "FAILED!!!!!" + arg0);
+					}
 
+					@Override
+					public void success(List<Customer> customerResponse,
+							Response response) {
+						textView.setText("Users: \n");
+						for (Customer c : customerResponse) {
+							Log.i("App", "ID: " + c.getId());
+							Log.i("App", "Name: " + c.getName());
+							Log.i("App", "Points: " + c.getPoints());
+							textView.append("ID: " + c.getId() + "\nName: "
+									+ c.getName() + "\nPoints: "
+									+ c.getPoints() + "\n\n");
+						}
+					}
+
+				});
+			}
 		});
+
 	}
 
 }
