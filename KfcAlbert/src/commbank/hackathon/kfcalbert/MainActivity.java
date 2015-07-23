7ef713136bf7bbf21cf6aab9d8e1d523eef9b145
niloyc <niloy.chowdhury@student.adelaide.edu.au>
2015-07-23 19:25:01 +0930

    Latest kfc albert

--- KfcAlbert/src/commbank/hackathon/kfcalbert/MainActivity.java
+++ KfcAlbert/src/commbank/hackathon/kfcalbert/MainActivity.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,8 +17,6 @@ import android.widget.ListView;
 public class MainActivity extends Activity implements OnClickListener{
 	
 	private static String tag = ".MainActivity";
-	private Thread pollingThread;
-	private Poller poller;
 	private Button confirmButton;
 	
 	private ListView orderList;
@@ -39,7 +36,6 @@ public class MainActivity extends Activity implements OnClickListener{
 		
 		getActionBar().hide();
 		
-		pollingThread = new Thread(poller = new Poller(mHandler));
 		orderList = (ListView) findViewById(R.id.itemList);
 		
 		List<OrderItem> items = new ArrayList<OrderItem>();
@@ -52,41 +48,14 @@ public class MainActivity extends Activity implements OnClickListener{
 		confirmButton.setOnClickListener(this);
 		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		pollingThread.start();
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 	
 	@Override
-	protected void onStop() {
-		poller.stop();
-		try{
-			pollingThread.join();
-		}catch (InterruptedException e){
-			e.printStackTrace();
-		}
-		
+	protected void onStop() {		
 		super.onStop();
 	}
 
@@ -95,7 +64,8 @@ public class MainActivity extends Activity implements OnClickListener{
 		int id = v.getId();
 		switch(id){
 		case R.id.btn_confirm_order:
-			Log.d(tag,"Order Confirmed");
+			Intent intent = new Intent(this, QRActivity.class);
+			startActivity(intent);
 			break;
 		}
 	}
--- KfcAlbert/src/commbank/hackathon/kfcalbert/QRActivity.java
+++ KfcAlbert/src/commbank/hackathon/kfcalbert/QRActivity.java
@@ -11,9 +11,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -24,49 +26,23 @@ import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 
-public class QRActivity extends Activity {
+public class QRActivity extends Activity implements OnClickListener{
 
+	private static String tag = ".QRActivity";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
         TextView myAwesomeTextView = (TextView)findViewById(R.id.scan_content);
         myAwesomeTextView.setText("-");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_qr, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Called upon payment button click.
-    public void onButtonClick(View view){
-        Log.d("mytag", "Button clicked.");
-        IntentIntegrator integrator = new IntentIntegrator(QRActivity.this);
-        integrator.initiateScan();
-        Log.d("mytag", "Button click function ended.");
+        ((Button)findViewById(R.id.generate_button)).setOnClickListener(this);
+        ((Button)findViewById(R.id.scan_button)).setOnClickListener(this);
     }
 
     // Callback function for scan result.
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        Log.d("mytag", "Activity result found.");
+        Log.d(tag, "Activity result found.");
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         String result = "Did not work.";
         if (scanResult != null) {
@@ -76,29 +52,40 @@ public class QRActivity extends Activity {
         myAwesomeTextView.setText(result);
     }
 
-    // Called upon payment button click.
-    public void onButtonClick2(View view){
-        Log.d("mytag", "Button2 clicked.");
-        String str = "a man's anus";
-        BitMatrix result;
-        try {
-            result = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 500, 500, null);
-        } catch (Exception ex) {
-            // Unsupported format
-            return;
-        }
-        //
-        int w = result.getWidth();
-        int h = result.getHeight();
-        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
-        for (int x = 0; x < w; x++){
-            for (int y = 0; y < h; y++){
-                bmp.setPixel(x, y, result.get(x,y) ? Color.BLACK : Color.WHITE);
-            }
-        }
-        ImageView qr_image = (ImageView) findViewById(R.id.image);
-        qr_image.setImageBitmap(bmp);
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch(id){
+		case R.id.scan_button:
+	        Log.d(tag, "Scan clicked.");
+	        IntentIntegrator integrator = new IntentIntegrator(this);
+	        integrator.initiateScan();
+	        Log.d(tag, "Scan function ended.");
+			break;
+		case R.id.generate_button:
+	        Log.d(tag, "Generate clicked.");
+	        String str = "a man's anus";
+	        BitMatrix result;
+	        try {
+	            result = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 500, 500, null);
+	        } catch (Exception ex) {
+	            // Unsupported format
+	            return;
+	        }
+	        //
+	        int w = result.getWidth();
+	        int h = result.getHeight();
+	        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+	        for (int x = 0; x < w; x++){
+	            for (int y = 0; y < h; y++){
+	                bmp.setPixel(x, y, result.get(x,y) ? Color.BLACK : Color.WHITE);
+	            }
+	        }
+	        ImageView qr_image = (ImageView) findViewById(R.id.image);
+	        qr_image.setImageBitmap(bmp);
 
-        Log.d("mytag", "Button2 click function ended.");
-    }
+	        Log.d(tag, "Generate click function ended.");
+			break;
+		}
+	}
 }
